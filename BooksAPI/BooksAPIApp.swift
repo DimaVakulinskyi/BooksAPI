c4f57e6a972a6e2a0ac56b90037ffd4f1621d4c6
Dmytro Vakulinskyi <d.vakulinskyi@gmail.com>
2023-07-03 14:11:46 +0300

    Initial Commit

---
+++ BooksAPI/BooksAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAPIApp.swift
+//  BooksAPI
+//
+//  Created by Дмитро Вакулінський on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BooksAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
